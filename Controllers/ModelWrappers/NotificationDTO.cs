a51c4383a733fbf89dd4ea9087b4aa6edee577b0
tvkoz <t.kozlov@nicetu.spb.ru>
2022-10-08 21:35:36 +0300

    Добавил сохранение уведомлений

---
+++ Controllers/ModelWrappers/NotificationDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MoretechBack.Controllers.ModelWrappers;
+
+public class NotificationDTO
+{
+    [JsonConstructor]
+    public NotificationDTO(
+        string owner,
+        string shortDescription,
+        string fullDescription
+    )
+    {
+        Owner = owner;
+        ShortDescription = shortDescription;
+        FullDescription = fullDescription;
+    }
+
+    public string Owner { get; }
+
+    public string ShortDescription { get; }
+    
+    public string FullDescription { get; }
+}
